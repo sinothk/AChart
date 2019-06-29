6f5317a44f41590af6b2e3559b5102a2f200c23c
LiangYT <381518188@qq.com>
2019-06-29 15:08:14 +0800

    加入Lib

--- app/src/main/java/com/sinothk/chart/demo/ChartDemoMainActivity.java
+++ app/src/main/java/com/sinothk/chart/demo/ChartDemoMainActivity.java
@@ -1,13 +1,19 @@
 package com.sinothk.chart.demo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ChartDemoMainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pieChartEvent(View view) {
+        startActivity(new Intent(ChartDemoMainActivity.this, PieChartActivity.class));
     }
 }
