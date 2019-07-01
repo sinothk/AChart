41b2add37fef20c4e2fa2c9e3f02afa04463d623
LiangYT <381518188@qq.com>
2019-07-01 15:07:07 +0800

    加入柱状图

---
+++ app/src/main/java/com/sinothk/chart/demo/AnyChartDemoMainActivity.java
@@ -0,0 +1,69 @@
+package com.sinothk.chart.demo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.sinothk.chart.demo.OXViewDemo.OXViewDemoMainActivity;
+
+public class AnyChartDemoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_demo);
+    }
+
+    public void oXViewChartsEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, OXViewDemoMainActivity.class));
+    }
+
+    public void pieChartEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, PieChartActivity.class));
+    }
+
+    public void columnChartEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, ColumnChartActivity.class));
+    }
+
+    public void lineChartEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, LineChartActivity.class));
+    }
+
+    public void paretoChartEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, ParetoChartActivity.class));
+    }
+
+    public void radarChartEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, RadarChartActivity.class));
+    }
+
+    public void funnelChartEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, FunnelChartActivity.class));
+    }
+
+    public void pyramidChartEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, PyramidActivity.class));
+    }
+
+    public void windSpeedEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, WindSpeedActivity.class));
+    }
+
+    public void pertChartEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, PertChartActivity.class));
+    }
+
+    public void verticalChartEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, VerticalChartActivity.class));
+    }
+
+    public void circularGaugeEvent(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, CircularGaugeActivity.class));
+    }
+
+    public void barChart1Event(View view) {
+        startActivity(new Intent(AnyChartDemoMainActivity.this, BarChartActivity.class));
+    }
+}
---
+++ app/src/main/java/com/sinothk/chart/demo/OXViewDemo/OXViewDemoMainActivity.java
@@ -0,0 +1,86 @@
+package com.sinothk.chart.demo.OXViewDemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.sinothk.chart.demo.OXViewDemo.adapter.CommandRecyclerAdapter;
+import com.sinothk.chart.demo.OXViewDemo.adapter.ViewHolder;
+import com.sinothk.chart.demo.OXViewDemo.charts.HorizontalBarActivity;
+import com.sinothk.chart.demo.OXViewDemo.charts.PieActivity;
+import com.sinothk.chart.demo.OXViewDemo.charts.VerticalBarActivity;
+import com.sinothk.chart.demo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// https://github.com/openXu/OXChart
+public class OXViewDemoMainActivity extends AppCompatActivity {
+
+
+    private RecyclerView recyclerView;
+    private List<String> datas;
+
+//    Handler handler = new Handler(){
+//        @Override
+//        public void handleMessage(Message msg) {
+//            super.handleMessage(msg);
+//        }
+//    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ox_view_chart_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        datas = new ArrayList<>();
+        datas.add("南丁格尔玫瑰图");
+        datas.add("饼状图");
+        datas.add("进度环形图");
+        datas.add("纵向柱状图");
+        datas.add("横向柱状图");
+        datas.add("折线图");
+        datas.add("股票信息");
+
+        recyclerView.setAdapter(new CommandRecyclerAdapter<String>(this, R.layout.activity_ox_view_list_item, datas) {
+            @Override
+            public void convert(ViewHolder holder, String str) {
+                holder.setText(R.id.tv_name, str);
+            }
+
+            @Override
+            public void onItemClick(String str, int position) {
+                switch (position) {
+//                    case 0:
+//                        startActivity(new Intent(MainActivity.this, RoseActivity.class));
+//                        break;
+                    case 1:
+                        startActivity(new Intent(OXViewDemoMainActivity.this, PieActivity.class));
+                        break;
+//                    case 2:
+//                        startActivity(new Intent(MainActivity.this, ProgressPieActivity.class));
+//                        break;
+                    case 3:
+                        startActivity(new Intent(OXViewDemoMainActivity.this, VerticalBarActivity.class));
+                        break;
+                    case 4:
+                        startActivity(new Intent(OXViewDemoMainActivity.this, HorizontalBarActivity.class));
+                        break;
+//                    case 5:
+//                        startActivity(new Intent(MainActivity.this, XmStockChartActivity.class));
+//                        break;
+//                    case 6:
+//                        startActivity(new Intent(MainActivity.this, XmStockChartActivity1.class));
+//                        break;
+                }
+            }
+
+        });
+
+    }
+}
